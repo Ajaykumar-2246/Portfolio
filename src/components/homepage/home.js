27f1ad7ad5f014c962d4caf62b0def20d981c173
Ajaykumar-2246 <131764062+Ajaykumar-2246@users.noreply.github.com>
2025-01-07 09:44:06 +0530

    My Portfolio

--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -57,7 +57,7 @@ function Home() {
               contact me
             </button>
           </Link>
-          <a href="https://drive.google.com/file/d/11Tmk5cFpbei1B3G8JSEUxYP9cxRhKobp/view"
+          <a href="https://drive.google.com/file/d/1iC9aT8nphYDJU6NECo5DRVxHwje7rJyg/view"
           target="_blank"
           rel="noopener noreferrer"
           >
